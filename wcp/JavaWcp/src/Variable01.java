18f56f8ad70f89190fd6f89ec6fda007a48040a0
sayakanoa <reiluv75418@gmail.com>
2022-01-24 16:05:14 +0900

    変数の練習

---
+++ wcp/JavaWcp/src/Variable01.java
@@ -0,0 +1,17 @@
+public class Variable01 {
+
+	public static void main(String[] args) {
+		int num1;
+		String str1;
+		num1 = 10;
+		str1 = "Hello World!";
+		System.out.println(num1);
+		System.out.println(str1);
+
+		num1 = 20;
+		System.out.println(num1);
+
+		int num2 = 30;
+		System.out.println(num2);
+	}
+}
