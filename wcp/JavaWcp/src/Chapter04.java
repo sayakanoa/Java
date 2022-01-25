f834f2d3147cc19e8135b032022fefac3ca75a9d
sayakanoa <reiluv75418@gmail.com>
2022-01-25 12:18:42 +0900

    変数・定数の練習問題

---
+++ wcp/JavaWcp/src/Chapter04.java
@@ -0,0 +1,13 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		String word = "sayaka";
+		System.out.println(word);
+
+		word = "一期一会";
+		System.out.println(word);
+
+		final int luckey_number = 7;
+		System.out.println(luckey_number);
+	}
+}
