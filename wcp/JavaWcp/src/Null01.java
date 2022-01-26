29ce18475f136379eef2e1e095a4887cea382378
sayakanoa <reiluv75418@gmail.com>
2022-01-26 17:36:02 +0900

    型の基本練習

---
+++ wcp/JavaWcp/src/Null01.java
@@ -0,0 +1,14 @@
+
+public class Null01 {
+	public static void main(String[] args) {
+		String string1;
+		
+		string1 = "あいう";
+		
+		System.out.println(string1 + "の文字数：" + string1.length());
+		
+		string1 = "";
+		System.out.println(string1 + "の文字数：" + string1.length());
+		
+	}
+}
