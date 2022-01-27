c8f9cbfb904855fb34c4f070f2efb26fd0120abf
sayakanoa <reiluv75418@gmail.com>
2022-01-27 21:41:40 +0900

    型の基本

---
+++ wcp/JavaWcp/src/Chapter05.java
@@ -0,0 +1,18 @@
+
+public class Chapter05 {
+	public static void main(String[] args) {
+		
+		long l = 10000000000L;
+		System.out.println(l);
+		
+		double d = 3.14;
+		System.out.println(d);
+		
+		boolean b = true;
+		System.out.println(b);
+		
+		int [] array = { 2 , 3 , 5 , 7 , 8 };
+		System.out.println(array[2]);
+		
+	}
+}
