25e89086179e5c7bde47b6dd812b4f8d04e615f9
sayakanoa <reiluv75418@gmail.com>
2022-01-29 22:20:04 +0900

    [fix]型の基本

--- wcp/JavaWcp/src/Chapter05.java
+++ wcp/JavaWcp/src/Chapter05.java
@@ -1,18 +1,27 @@
 
 public class Chapter05 {
 	public static void main(String[] args) {
-		
+
 		long l = 10000000000L;
 		System.out.println(l);
-		
+
 		double d = 3.14;
 		System.out.println(d);
-		
+
 		boolean b = true;
 		System.out.println(b);
-		
+
+		Integer i = 100;
+		System.out.println(i);
+
+		Float f = 1.4142f;
+		System.out.println(f);
+
+		Character c = 'う';
+		System.out.println(c);
+
 		int [] array = { 2 , 3 , 5 , 7 , 8 };
 		System.out.println(array[2]);
-		
+
 	}
 }
