ce0b51e0314150d15ce65041cac55f2c6718c09e
sayakanoa <reiluv75418@gmail.com>
2022-01-30 12:43:48 +0900

    ループ文練習問題

---
+++ wcp/JavaWcp/src/Exam11.java
@@ -0,0 +1,17 @@
+
+public class Exam11 {
+	public static void main(String[] args) {
+		int count = 1;
+	    int result = 0;
+
+	    while(count <= 100){
+	          result += count;
+	          count++;
+	      }
+	      System.out.println("合計は" + result + "です");
+
+
+	}
+
+
+}
