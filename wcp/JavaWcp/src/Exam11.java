a40ff00f604745111a83c68671d38495be745b0e
sayakanoa <reiluv75418@gmail.com>
2022-01-30 12:55:31 +0900

    for文を使った練習問題

--- wcp/JavaWcp/src/Exam11.java
+++ wcp/JavaWcp/src/Exam11.java
@@ -1,15 +1,15 @@
 
 public class Exam11 {
 	public static void main(String[] args) {
-		int count = 1;
-	    int result = 0;
 
-	    while(count <= 100){
-	          result += count;
-	          count++;
-	      }
-	      System.out.println("合計は" + result + "です");
 
+		int result = 0;
+
+	    for (int i=1; i <= 100; i++) {
+
+	    	result += i;
+	    }
+	    System.out.println("合計値は" + result);
 
 	}
 
